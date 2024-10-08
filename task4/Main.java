06dbbdf2dd5684bca97c798bbc785f69f409f5cf
Mirajane <151534082+MaryShust@users.noreply.github.com>
2024-10-08 23:58:37 +0300

    Create Main.java

---
+++ task4/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        String name = "World";
+        System.out.println(greeter.greet(name));
+        Utils.utilsMethod();
+    }
+}
